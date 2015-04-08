fixed load precision bug and getting tests working

networkbuild/network_io/__init__.py
@@ -28,7 +28,7 @@ def load_shp(shp_path):
     # NOTE:  if shp_path is unicode io doesn't work for some reason
     shp_path = shp_path.encode('ascii', 'ignore')
     g = nx.read_shp(shp_path)
-    coords = np.array(g.nodes())
+    coords = dict(enumerate(g.nodes()))
 
     driver = ogr.GetDriverByName('ESRI Shapefile')
     shp = driver.Open(shp_path)

networkbuild/networker.py
@@ -49,13 +49,15 @@ class Networker(object):
 
         geo_graph = subgraphs = rtree = None
 
-        demand_nodes = Networker.load_node_metrics(**self.config['demand_nodes'])
+        demand_nodes = load_node_metrics(**self.config['demand_nodes'])
         if self.config.has_key('existing_networks'): 
-            existing = Networker.load_existing_networks(**self.config['existing_networks'])
+            existing = load_existing_networks(**self.config['existing_networks'])
             # rename existing nodes so that they don't intersect with metrics 
-            existing = nx.relabel_nodes(existing, \
-                {n: 'grid-' + str(n) for n in existing.nodes()})
-            geo_graph, subgraphs, rtree = Networker.\
+            nx.relabel_nodes(existing, \
+                {n: 'grid-' + str(n) for n in existing.nodes()}, copy=False)
+            existing.coords = {'grid-' + str(n): c for n, c in \
+                existing.coords.items()}
+            geo_graph, subgraphs, rtree = \
                 merge_network_and_nodes(existing, demand_nodes)
         else:
             geo_graph = demand_nodes
@@ -80,158 +82,10 @@ class Networker(object):
         coords = {i: result_geo_graph.coords[i] for i in filtered_graph}
         geo = GeoGraph(result_geo_graph.srs, coords=coords, data=filtered_graph)
 
-        import pdb; pdb.set_trace()
         # now save it
         nio.write_shp(geo, self.config['output_directory'])
-            
-    @staticmethod
-    def merge_network_and_nodes(network, demand_nodes):
-        """
-        merge the network and nodes GeoGraphs to set up the Graph, UnionFind 
-        (DisjoinSet), and RTree datastructures for use in network algorithms
-
-        Args:
-            network:  graph representing existing network (assumes node ids don't conflict
-                with net (demand) nodes)
-            demand_nodes:  graph of nodes representing demand
-
-        Returns:
-            graph:  graph with demand nodes and their nearest nodes to the 
-                existing network (i.e. 'fake' nodes)
-            subgraphs:  UnionFind datastructure populated with fake nodes and
-                associated with the appropriate connected component from the
-                network
-            rtree:  spatial index populated with the edges from the 
-                existing network 
-        """
-        
-        # project demand nodes onto network
-        rtree = network.get_rtree_index()
-        grid_with_fakes = network.project_onto(demand_nodes, rtree_index=rtree)
-        
-        # get only the fake nodes and the associated network edges
-        demand_node_set = set(demand_nodes.nodes())
-        net_plus_demand = set(network.nodes()) + demand_node_set
-        fakes = set(grid_with_fakes.nodes()) - net_plus_demand_
-        # fake node should only have 2 neighbors from the existing network
-        # that is the nearest edge
-        get_fake_edge = lambda node: tuple(set(grid_with_fakes.neighbors(x)) - \
-            demand_node_set)
-        edge_fakes = [(get_fake_edge(fake), fake) for fake in fakes]
-
-        # Get the network components to init budget centers
-        subnets = nx.connected_components(network)
-
-        # Init the DisjointSet
-        subgraphs = UnionFind()
-
-        # Build the subnet components
-        for sub in subnets:
-            # Start subcomponent with first node
-            subgraphs.add_component(sub[0], budget=network.node[sub[0]]['budget'])
-            # Merge remaining nodes with component
-            for node in sub[1:]:
-                subgraphs.add_component(node, budget=network.node[node]['budget'])
-                # The existing grid nodes are on the grid (so distance is 0)
-                subgraphs.union(sub[0], node, 0)
-
-        # setup merged graph to be populated with fake nodes
-        merged = GeoGraph(demand_nodes.srs, demand_nodes.coords)
-        # merge fakes in
-        for ((u, v), fake) in edge_fakes:
-
-            # Make sure something wonky isn't going on
-            assert(subgraphs[u] == subgraphs[v])
-
-            # Add the fake node to the big net
-            merged.add_node(fake, budget=np.inf)
-            merged.coords[fake] = grid_with_fakes.coords[fake]
-
-            # Merge the fake node with the grid subgraph
-            subgraphs.add_component(fake, budget=np.inf)
-            subgraphs.union(fake, u, 0)
-
-        return merged, subgraphs, rtree
-
-
-    @staticmethod
-    def load_existing_networks(filename="existing_networks.shp", budget_value=0):
-        """
-        load existing_networks shp into GeoGraph nodes, edges and assign budget 
-
-        Args:
-            filename:  existing_networks shapefile
-            budget_value:  default budget value for nodes in existing network
-        
-        Returns:
-            GeoGraph of existing networks with budget attribute
-        """
- 
-        geo_net = nio.load_shp(filename)
-        
-        nx.set_node_attributes(geo_net, 'budget', {n:budget_value for n in geo_net.nodes()})
-
-        # determine edge weight/distance function by whether they're geocentric
-        # (assuming coordinates are not in 3-space here)
          
-        distance = gm.spherical_distance if geo_net.is_geographic else \
-            gm.euclidean_distance 
-
-        nx.set_edge_attributes(geo_net, 'weight', {(u, v): \
-                       distance(map(geo_net.coords.get, [u,v]))  \
-                       for u,v in geo_net.edges()}) 
-
-        return geo_net
-
-
-    @staticmethod
-    def load_node_metrics(filename="metrics.csv", x_column="X", y_column="Y", \
-        budget_column="metric", budget_value=1000):
-        """
-        load node_metrics csv into GeoGraph (nodes only)
-
-        Args:
-            filename:  nodal metrics csv file
-            x_column, y_column:  col names to take x, y from
-            budget_column:  col to take budget from
-            budget_value: default value for nodal budget
-        
-        Returns:
-            GeoGraph of nodes only with budget attribute
-        """
-    
-        input_proj = csv_projection(filename)
-        header_row = 1 if input_proj else 0
-
-        # read in the csv
-        metrics = pd.read_csv(filename, header=header_row)
-
-        coord_cols = [x_column, y_column]
-        assert all([hasattr(metrics, col) for col in coord_cols]), \
-            "metrics file does not contain coordinate columns {}, {}".\
-            format(x_column, y_column)
-
-        # default budget
-        budget = len(metrics) * [budget_value]
-        # try to get nodal budgets
-        if hasattr(metrics, budget_column):
-            budget = metrics[budget_column].fillna(budget_value)
-
-        # Stack the coords and get the budget
-        coords = np.column_stack(map(metrics.get, coord_cols))
-
-        to_dict = lambda column: {i: value for i, value in enumerate(column)}
-        coords_dict = dict(enumerate(coords))
-        budget_dict = dict(enumerate(budget))
-
-        geo_nodes = GeoGraph(input_proj if input_proj else gm.PROJ4_FLAT_EARTH, coords_dict)
-        
-        nx.set_node_attributes(geo_nodes, 'budget', budget_dict)
-        return geo_nodes
-       
-
-    @staticmethod
-    def _validate(config):
+    def _validate(self, config):
         """
         validate configuration
         throws jsonschema Validate exception if invalid
@@ -241,4 +95,158 @@ class Networker(object):
         schema_path = os.path.join(os.path.dirname(\
             os.path.abspath(__file__)), Networker.SCHEMA_FILE)
         schema = json.load(open(schema_path))
-        jsonschema.validate(config, schema)
+        jsonschema.validate(self.config, schema)   
+
+
+def merge_network_and_nodes(network, demand_nodes):
+    """
+    merge the network and nodes GeoGraphs to set up the Graph, UnionFind 
+    (DisjoinSet), and RTree datastructures for use in network algorithms
+
+    Args:
+        network:  graph representing existing network (assumes node ids don't conflict
+            with net (demand) nodes)
+        demand_nodes:  graph of nodes representing demand
+
+    Returns:
+        graph:  graph with demand nodes and their nearest nodes to the 
+            existing network (i.e. 'fake' nodes)
+        subgraphs:  UnionFind datastructure populated with fake nodes and
+            associated with the appropriate connected component from the
+            network
+        rtree:  spatial index populated with the edges from the 
+            existing network 
+    """
+    
+    # project demand nodes onto network
+    rtree = network.get_rtree_index()
+    grid_with_fakes = network.project_onto(demand_nodes, rtree_index=rtree)
+    
+    # get only the fake nodes and the associated network edges
+    demand_node_set = set(demand_nodes.nodes())
+    net_plus_demand = set(network.nodes()).union(demand_node_set)
+    fakes = set(grid_with_fakes.nodes()) - net_plus_demand
+    # fake node should only have 2 neighbors from the existing network
+    # that is the nearest edge
+    get_fake_edge = lambda node: tuple(set(grid_with_fakes.neighbors(node)) - \
+        demand_node_set)
+    edge_fakes = [(get_fake_edge(fake), fake) for fake in fakes]
+
+    # Get the network components to init budget centers
+    subnets = nx.connected_components(network)
+
+    # Init the DisjointSet
+    subgraphs = UnionFind()
+
+    # Build the subnet components
+    for sub in subnets:
+        # Start subcomponent with first node
+        subgraphs.add_component(sub[0], budget=network.node[sub[0]]['budget'])
+        # Merge remaining nodes with component
+        for node in sub[1:]:
+            subgraphs.add_component(node, budget=network.node[node]['budget'])
+            # The existing grid nodes are on the grid (so distance is 0)
+            subgraphs.union(sub[0], node, 0)
+
+    # setup merged graph to be populated with fake nodes
+    merged = GeoGraph(demand_nodes.srs, demand_nodes.coords, data=demand_nodes)
+    # merge fakes in
+    for ((u, v), fake) in edge_fakes:
+
+        # Make sure something wonky isn't going on
+        assert(subgraphs[u] == subgraphs[v])
+
+        # Add the fake node to the big net
+        merged.add_node(fake, budget=np.inf)
+        merged.coords[fake] = grid_with_fakes.coords[fake]
+
+        # Merge the fake node with the grid subgraph
+        subgraphs.add_component(fake, budget=np.inf)
+        subgraphs.union(fake, u, 0)
+
+    return merged, subgraphs, rtree
+
+
+def load_existing_networks(filename="existing_networks.shp", budget_value=0):
+    """
+    load existing_networks shp into GeoGraph nodes, edges and assign budget 
+
+    Args:
+        filename:  existing_networks shapefile
+        budget_value:  default budget value for nodes in existing network
+    
+    Returns:
+        GeoGraph of existing networks with budget attribute
+    """
+
+    geo_net = nio.load_shp(filename)
+    
+    nx.set_node_attributes(geo_net, 'budget', {n:budget_value for n in geo_net.nodes()})
+
+    # determine edge weight/distance function by whether they're geocentric
+    # (assuming coordinates are not in 3-space here)
+     
+    distance = gm.spherical_distance if geo_net.is_geographic else \
+        gm.euclidean_distance 
+
+    nx.set_edge_attributes(geo_net, 'weight', {(u, v): \
+                   distance(map(geo_net.coords.get, [u,v]))  \
+                   for u,v in geo_net.edges()}) 
+
+    return geo_net
+
+
+def load_node_metrics(filename="metrics.csv", x_column="X", y_column="Y", \
+    budget_column="metric", budget_value=1000):
+    """
+    load node_metrics csv into GeoGraph (nodes only)
+
+    Args:
+        filename:  nodal metrics csv file
+        x_column, y_column:  col names to take x, y from
+        budget_column:  col to take budget from
+        budget_value: default value for nodal budget
+    
+    Returns:
+        GeoGraph of nodes only with budget attribute
+    """
+
+    input_proj = csv_projection(filename)
+    header_row = 1 if input_proj else 0
+
+    # read in the csv
+    # NOTE:  Convert x,y via float cast to preserve precision of input
+    metrics = pd.read_csv(filename, header=header_row, \
+        converters={x_column: float, y_column: float})
+
+    coord_cols = [x_column, y_column]
+    assert all([hasattr(metrics, col) for col in coord_cols]), \
+        "metrics file does not contain coordinate columns {}, {}".\
+        format(x_column, y_column)
+
+    # default budget
+    budget = len(metrics) * [budget_value]
+    # try to get nodal budgets
+    if hasattr(metrics, budget_column):
+        budget = metrics[budget_column].fillna(budget_value)
+
+    # Stack the coords and get the budget
+    coords = np.column_stack(map(metrics.get, coord_cols))
+
+    # set default projection
+    if not input_proj:
+        if gm.is_in_lon_lat(coords):
+            input_proj= gm.PROJ4_LATLONG
+        else:
+            input_proj= gm.PROJ4_FLAT_EARTH
+
+    to_dict = lambda column: {i: value for i, value in enumerate(column)}
+    coords_dict = dict(enumerate(coords))
+    budget_dict = dict(enumerate(budget))
+
+    geo_nodes = GeoGraph(input_proj, coords_dict)
+    
+    nx.set_node_attributes(geo_nodes, 'budget', budget_dict)
+    return geo_nodes
+   
+

networkbuild/test/TestNetworkBuild.py
@@ -21,12 +21,20 @@ def test_networker_run():
     nwk = networker.Networker(cfg)
     nwk.run()
 
-    # compare this run against existing
+    # compare this run against existing results
     test_geo = network_io.load_shp(os.path.join(cfg['output_directory'], \
         "edges.shp"))
     known_geo = network_io.load_shp("data/max_100/networks-proposed.shp")
+    # compare sets of edges
+
+    test_edges = test_geo.get_coord_edge_set()
+    known_edges = known_geo.get_coord_edge_set()
+
+    assert test_edges == known_edges, \
+        "edges in test do not match known results"
+
     assert nx.is_isomorphic(test_geo, known_geo), \
-        "test results do not match known"
+        "test result graph is not isomorphic to known result graph"
 
     
 def random_settlements(n):
@@ -109,16 +117,13 @@ def nodes_plus_existing_grid():
     """
 
     # setup grid
-    grid_coords = np.array([[-5, 0], [5, 0]])
-    grid = nx.Graph()
-    grid.add_nodes_from(range(2))
-    nx.set_node_attributes(grid, 'coords', dict(enumerate(grid_coords)))
+    grid_coords = np.array([[-5.0, 0.0], [5.0, 0.0]])
+    grid = cls.GeoGraph(gm.PROJ4_FLAT_EARTH, {'grid-' + str(n): c for n, c in enumerate(grid_coords)})
     nx.set_node_attributes(grid, 'budget', {n:0 for n in grid.nodes()})
-    grid.add_edges_from([(0, 1)])
-    grid = nx.relabel_nodes(grid, {n: 'grid-' + str(n) for n in grid.nodes()})
+    grid.add_edges_from([('grid-0', 'grid-1')])
 
     # setup input nodes
-    node_coords = np.array([[0, 2], [-1, 4], [4, 1]])
+    node_coords = np.array([[0.0, 2.0], [-1.0, 4.0], [4.0, 1.0]])
     nodes = cls.GeoGraph(gm.PROJ4_FLAT_EARTH, dict(enumerate(node_coords)))
     budget_values = [2, 3, 5]
     nx.set_node_attributes(nodes, 'budget', dict(enumerate(budget_values)))
@@ -139,8 +144,9 @@ def test_msf_behavior():
 
     for n, _ in enumerate(nodes.node):
         sub = nodes.subgraph(range(n+1))
+        sub.coords = {i: nodes.coords[i] for i in range(n+1)}
         G, DS, R = networker.merge_network_and_nodes(grid, sub)
-        msf = mod_boruvka(G, DS, R, spherical_coords=False)
+        msf = mod_boruvka(G, DS, R)
         msf_sets = set([frozenset(e) for e in msf.edges()])
         iter_edge_set = set([frozenset(e) for e in edges_at_iteration[n]])
         eq_(msf_sets, iter_edge_set)