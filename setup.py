67b90497670a50273d1a68bfc3cc7b6062657f8f
chrisnatali <chris.natali@gmail.com>
2015-05-01 15:29:05 -0400

    v0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="networker",
-    version="0.0.7",
+    version="0.0.8",
     packages=find_packages(),
     description="Python library for planning distribution networks",
     long_description=open("README.md").read(),
